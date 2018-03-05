993be12bfe8e70305f6ab66f5552fc0dfffcd64f
arczi92r <arczi92r@hotmail.com>
2018-03-05 21:25:54 +0100

    chary pick

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,10 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Data.SqlClient;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Data;
 using DB.Repository;
 
 namespace ConsoleApp1
